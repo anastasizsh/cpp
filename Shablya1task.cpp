157ac762b3e4628f9f45d7c913bb305cac40f6e7
anastasizsh <115626775+anastasizsh@users.noreply.github.com>
2023-09-12 22:57:37 +0300

    Add files via upload

---
+++ Shablya1task.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <math.h>
+
+int main() 
+{
+    int a=2;
+    double b=200;
+    double result;
+    result = pow( (11.21*(1+log(b/a)))/((1-sin((a/b)*M_PI))*(cos(a/b)*M_PI)), 0.67);
+
+  std:: cout<< result;
+  
+}
---
+++ shablya2task.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <math.h>
+
+int main() 
+{
+    int a=3;
+    double b=0.707;
+    double result;
+    result = pow((0.5*sin((log(pow((M_E) ,(b+a)))*(M_PI /8)))+(1,308*cos((log(pow(M_E,(a-b)))*(M_PI/8))))),0.67);
+
+  std:: cout<< result;
+  
+}
