f7e4c6878c19f7e11d14f831e77c0e08aa3c1b08
anastasizsh <115626775+anastasizsh@users.noreply.github.com>
2024-02-29 21:26:14 +0200

    Add files via upload

---
+++ pz14/pz14.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(void) {
+    int vector[] = {3, -5, 7, 10, -4, 14, 5, 2, -13};
+    int n = sizeof(vector) / sizeof(vector[0]);
+    int *ptr = vector;
+    int max_element = *ptr;
+    for (int i = 1; i < n; ++i) {
+   
+        if (*(ptr + i) > max_element) {
+            max_element = *(ptr + i);
+        }
+    }
+    std::cout << "Максимальний елемент: " << max_element << std::endl;
+
+    return 0;
+}
